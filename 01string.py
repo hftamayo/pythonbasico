e79e4a7de3ec642e3df5750a9b2802aa26029290
htamayo <hftamayo@gmail.com>
2020-04-17 09:33:22 -0500

    thecybermenthor hasta tuto 2

---
+++ 01string.py
@@ -0,0 +1,10 @@
+#! /bin/python3
+
+#imprimir un string
+print("Primer script con Python")
+print("\n")
+print("""La funcion print en Python
+acepta impresion en multilinea""")
+print(" y tambien concatenado " + " usando el simbolo +")
+
+
---
+++ 02math.py
@@ -0,0 +1,12 @@
+#! /bin/python3
+
+print("PEMDAS: ")
+print(50 + 50 - 50 * 50 / 50)
+print("Exponente: ") 
+print(50 ** 2)
+print("Modulo: ")
+print(50 * 6)
+print("Cociente: ")
+print( 50 / 6)
+print("Cociente redondeado: ")
+print( 50 // 6)
